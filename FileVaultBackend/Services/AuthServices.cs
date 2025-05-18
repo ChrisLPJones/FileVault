cc79caf905c416074d874da95fc897024ce3b590
Chris LP Jones <crispyjonesdev@outlook.com>
2025-05-19 09:17:37 +1000

    Update AuthServices

--- FileVaultBackend/Services/AuthServices.cs
+++ FileVaultBackend/Services/AuthServices.cs
@@ -1,7 +1,21 @@
-﻿namespace FileVaultBackend.Services
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+
+namespace FileVaultBackend.Services
 {
     public class AuthServices
     {
+        // Add user to database
 
+        // Delete user
+        
+        // Get password hash
+
+        // Get JWT tokes
+
+        // update token
+
+        // log user out
+
+        // Get user info
     }
 }
