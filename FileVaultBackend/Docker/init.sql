6215e21747c8e332f6b7573d00f88c6884f9ee3f
chrislpjones <chrislpjones.dev@outlook.com>
2025-05-26 17:05:54 +1000

    Updated SQL Init

--- FileVaultBackend/Docker/init.sql
+++ FileVaultBackend/Docker/init.sql
@@ -1,3 +1,4 @@
+-- Create database if it doesn't exist
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'SecureVaultDb')
 BEGIN
     CREATE DATABASE SecureVaultDb;
@@ -7,49 +8,49 @@ ELSE
 BEGIN
     PRINT 'Database "SecureVaultDb" already exists.';
 END
+GO
 
-DECLARE @file NVARCHAR(MAX) = '
 USE SecureVaultDb;
+GO
 
-IF OBJECT_ID(''Files'', ''U'') IS NULL
+-- Drop Files table if it exists (must drop this first due to FK to Users)
+IF OBJECT_ID('Files', 'U') IS NOT NULL
 BEGIN
-    CREATE TABLE Files (
-        Id INT IDENTITY(1,1) PRIMARY KEY,
-        FileName NVARCHAR(50),
-        GUID NVARCHAR(50)
-    );
-    ALTER TABLE Files
-    ADD CONSTRAINT UQ_Files_GUID UNIQUE (GUID);
-    PRINT ''Table "Files" created.'';
+    DROP TABLE Files;
+    PRINT 'Table "Files" dropped.';
 END
-ELSE
+GO
+
+-- Drop Users table if it exists
+IF OBJECT_ID('Users', 'U') IS NOT NULL
 BEGIN
-    PRINT ''Table "Files" already exists.'';
+    DROP TABLE Users;
+    PRINT 'Table "Users" dropped.';
 END
-';
+GO
 
-EXEC(@file);
+-- Recreate Users table
+CREATE TABLE Users (
+    Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    PasswordHash VARCHAR(255) NOT NULL,
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    LastLogin DATETIME,
+    Role VARCHAR(20) DEFAULT 'user'
+);
+PRINT 'Table "Users" created.';
+GO
 
-DECLARE @users NVARCHAR(MAX) = '
-USE SecureVaultDb;
-
-IF OBJECT_ID(''Users'', ''U'') IS NULL
-BEGIN
-    CREATE TABLE Users (
-        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-        Username VARCHAR(50) UNIQUE NOT NULL,
-        Email VARCHAR(100) UNIQUE NOT NULL,
-        PasswordHash VARCHAR(255) NOT NULL,
-        CreatedAt DATETIME DEFAULT GETDATE(),
-        LastLogin DATETIME,
-        Role VARCHAR(20) DEFAULT ''user''
-    );
-    PRINT ''Table "Users" created.'';
-END
-ELSE
-BEGIN
-    PRINT ''Table "Users" already exists.'';
-END
-';
-
-EXEC(@users);
+-- Recreate Files table with FK to Users
+CREATE TABLE Files (
+    Id INT IDENTITY(1,1) NOT NULL,
+    FileName NVARCHAR(50) NOT NULL,
+    GUID NVARCHAR(50) NOT NULL,
+    UserId UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT PK_Files PRIMARY KEY CLUSTERED (Id ASC),
+    CONSTRAINT UQ_Files_GUID UNIQUE NONCLUSTERED (GUID ASC),
+    CONSTRAINT FK_Files_Users FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+PRINT 'Table "Files" created with constraints.';
+GO
