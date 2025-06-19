81fc683784db59fb2f682a70097b1864f68a7fb0
Chris LP Jones <crispyjonesdev@outlook.com>
2025-06-20 00:52:31 +1000

    Added Register User Test

--- Backend.Test/FileEndpoints.Test.cs
+++ Backend.Test/FileEndpoints.Test.cs
@@ -1,8 +1,9 @@
-﻿using FluentAssertions;
+﻿using Backend.Models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
-using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 
 namespace Backend.Test
 {
@@ -23,45 +24,88 @@ namespace Backend.Test
         [Fact]
         public async Task Upload_List_Download_Delete_WithValidRequest_ReturnsSuccess()
         {
-            /* Upload */
+            /* Register User */
+
             // Arrange
-            var fileContent = new ByteArrayContent(Encoding.UTF8.GetBytes("Dummy file content"));
-            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("text/plain");
+            UserModel user = new()
+            {
+                Username = "testuser",
+                Email = "testemail@address.com",
+                Password = "testpassword"
+            };
 
-            var multipartContent = new MultipartFormDataContent();
-            multipartContent.Add(fileContent, name: "file", fileName: "test.txt");
-
-            // Act
-            var response = await _client.PostAsync("/upload", multipartContent);
-
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var content = await response.Content.ReadAsStringAsync();
-            content.Should().Be("\"File Uploaded: test.txt\"");
-            /* List */
+            string json = JsonSerializer.Serialize(user);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
             // Act
-            response = await _client.GetAsync("/files");
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            content = await response.Content.ReadAsStringAsync();
-            content.Should().Contain("test.txt");
+            HttpResponseMessage response = await _client.PostAsync("/user/register", content);
+            string responseBody = await response.Content.ReadAsStringAsync();
 
-            /* Download */
-            // Act
-            response = await _client.GetAsync("download/test.txt");
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            content = await response.Content.ReadAsStringAsync();
-            content.Should().Be("Dummy file content");
+            /// Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            responseBody.Should().Be("\"Successfully added user testuser\"");
 
-            /* Delete */
-            // Act
-            response = await _client.DeleteAsync("/delete/test.txt");
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            content = await response.Content.ReadAsStringAsync();
-            content.Should().Be("\"File deleted: test.txt\"");
+
+            /* Login User */
+
+            /* Get User */
+
+            /* Update User */
+
+            /* Delete User */
+
+
+
+
+            // /* Upload */
+
+            // Arrange
+            // var fileContent = new ByteArrayContent(Encoding.UTF8.GetBytes("Dummy file content"));
+            // fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("text/plain");
+
+            // var multipartContent = new MultipartFormDataContent();
+            // multipartContent.Add(fileContent, name: "file", fileName: "test.txt");
+
+            // // Act
+            // var response = await _client.PostAsync("/upload", multipartContent);
+
+            // // Assert
+            // response.StatusCode.Should().Be(HttpStatusCode.OK);
+            // var content = await response.Content.ReadAsStringAsync();
+            // content.Should().Be("\"File Uploaded: test.txt\"");
+            // /* List */
+
+            // // Act
+            // response = await _client.GetAsync("/files");
+
+            // // Assert
+            // response.StatusCode.Should().Be(HttpStatusCode.OK);
+            // content = await response.Content.ReadAsStringAsync();
+            // content.Should().Contain("test.txt");
+
+            // /* Download */
+
+            // // Act
+            // response = await _client.GetAsync("download/test.txt");
+
+            // // Assert
+            // response.StatusCode.Should().Be(HttpStatusCode.OK);
+            // content = await response.Content.ReadAsStringAsync();
+            // content.Should().Be("Dummy file content");
+
+            // /* Delete */
+
+            // // Act
+            // response = await _client.DeleteAsync("/delete/test.txt");
+
+            // // Assert
+            // response.StatusCode.Should().Be(HttpStatusCode.OK);
+            // content = await response.Content.ReadAsStringAsync();
+            // content.Should().Be("\"File deleted: test.txt\"");
+
+            /* Delete User */
+
+
         }
     }
 }
